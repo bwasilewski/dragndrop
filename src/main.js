3f2e476d0518854fc99ede351a86d96de9b335fa
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2018-05-01 21:10:38 -0500

    drop some html into an element

--- src/main.js
+++ src/main.js
@@ -1,14 +1,52 @@
 export default class DnD {
     constructor (el) {
+        let that = this
         this.el = el;
         this.items = Array.from(this.el.children)
 
         this.items.forEach(item => {
-            item.addEventListener('dragstart', ev => {
-                console.log('Event: ', ev)
-            }, false)
+            that.setupitem(item)
         })
         
         console.log('New DragNDrop instance created: ', el);
+    }
+
+    setupitem(item) {
+        // create parent element and make it droppable
+        let newparent = document.createElement('div')
+        item.parentNode.insertBefore(newparent, item)
+        newparent.appendChild(item)
+        // make item draggable
+        item.setAttribute('draggable', 'true')
+
+        // set up event handlers
+        item.addEventListener('dragstart', this.handledragstart)
+        item.addEventListener('dragenter', this.handledragenter)
+        item.addEventListener('dragover', this.handledrag)
+        item.addEventListener('drop', this.handledrop)
+
+        newparent.addEventListener('dragover', ev => {ev.preventDefault()})
+        newparent.addEventListener('dragenter', ev => {ev.preventDefault()})
+    }
+
+    handledragstart(ev) {
+        ev.dataTransfer.setData('text', '<div>Hello there</div>')
+    }
+
+    handledragenter(ev) {
+        let target = ev.toElement.parentNode;
+
+        console.log('Target: ', target)
+    }
+
+    handledrag(ev) {
+        // console.log('Drag: ', ev)
+    }
+
+    handledrop(ev) {
+        console.log('Drop: ', ev)
+        let target = ev.toElement.parentNode
+        target.innerHTML = ev.dataTransfer.getData('text')
+        
     }
 }
